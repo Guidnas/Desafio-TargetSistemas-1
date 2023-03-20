b68ce0b6318ccc7b4804181e8eb1a3f099b4d424
Guilherme <Guilherme@WIN-EEPJK5B50AL>
2023-03-20 00:01:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+    K = K + 1;
+
+    SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA);
+
+
+
+Ao final do processamento, qual será o valor da variável SOMA?
+*/
+
+
+
+namespace Questao1 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13, SOMA = 0, K = 0;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+                SOMA = SOMA + K;
+            }
+
+            Console.WriteLine(SOMA);
+        }
+    }
+
+}
